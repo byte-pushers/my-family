8ecdf4ba14f64fe213114adaa41178f92123523a
MrHonor <danielamezquita5101@gmail.com>
2024-09-16 00:06:49 -0500

    feat(api): added .ts files for account creating/login api services

--- client/my-family-app/src/app/tab1/tab1.page.ts
+++ client/my-family-app/src/app/tab1/tab1.page.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { LoginApiService } from '../services/login-api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, HttpClientModule, IonButton],
 })
 export class Tab1Page {
-  constructor() {}
+  // constructor(private loginApi: LoginApiService) { }
+  //
+  // loginUser() {
+  //   const credentials = { email: 'test@test.com', password: 'test' };
+  //   this.loginApi.login(credentials).subscribe(
+  //     response => {
+  //       console.log('Login successful', response);
+  //     },
+  //     error => console.log('Login failed', error)
+  //   );
+  // }
 }
+
+
