57ce47f668922868d03259fe3599d77e94d37247
Adish Timalsina <adishtimalsina11@gmail.com>
2024-09-18 13:22:22 -0500

    feat(create-account-api created)

---
+++ server/my-family-ws/src/main/java/com/bytepushers/family/createaccount/CreateAccountController.java
@@ -0,0 +1,24 @@
+package com.bytepushers.family.createaccount;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CreateAccountController {
+
+    //test api
+    @GetMapping("/api/v1/home")
+    public String home() {
+        return "welcome to home";
+    }
+
+    //create-account post api
+    @PostMapping("/api/v1/create-account")
+    public String createAccount(
+            @RequestBody CreateAccount createAccount
+    ) {
+        return "your account has been created"+ createAccount.toString();
+    }
+}
