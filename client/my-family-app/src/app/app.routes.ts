5d6896e1861376f14b2e5e64eea03d747ce4eeb0
Tonté Pouncil <tonte.pouncil@bytepushers.software>
2024-09-26 20:07:59 -0500

    feat(login-view-2): Fixed bug in code that pervent other routes from displaying

--- client/my-family-app/src/app/app.routes.ts
+++ client/my-family-app/src/app/app.routes.ts
@@ -5,10 +5,10 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
- /* {
+   {
     path: 'create-account',
     loadComponent: () => import('./create-account/create-account.page').then( m => m.CreateAccountPage)
-  },*/
+  },
   {
     path: 'login-page',
     loadComponent: () => import('./login-page/login-page.page').then(m => m.LoginPagePage)
