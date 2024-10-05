13f045f746823a51c6f2219af48673e1f1243bab
Stella Choi <stellachoi120701@gmail.com>
2024-10-05 00:59:45 -0500

    feat(add-family-page-3): Created branch and started building page

---
+++ client/my-family-app/src/app/add-family-step-3/add-family-step-3.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AddFamilyStep3Page } from './add-family-step-3.page';
+
+describe('AddFamilyStep3Page', () => {
+  let component: AddFamilyStep3Page;
+  let fixture: ComponentFixture<AddFamilyStep3Page>;
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddFamilyStep3Page);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- client/my-family-app/src/app/app.routes.ts
+++ client/my-family-app/src/app/app.routes.ts
@@ -4,9 +4,14 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
-  },  {
+  },
+  {
     path: 'create-account',
     loadComponent: () => import('./create-account/create-account.page').then( m => m.CreateAccountPage)
   },
+  {
+    path: 'add-family-step-3',
+    loadComponent: () => import('./add-family-step-3/add-family-step-3.page').then( m => m.AddFamilyStep3Page)
+  }
 
 ];
