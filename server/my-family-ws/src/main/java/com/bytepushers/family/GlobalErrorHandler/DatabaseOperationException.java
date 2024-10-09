de0ab9d281ba84b77a300133b31a0ef7978f313f
eva tchicaya <75638773+evatchicaya@users.noreply.github.com>
2024-10-08 19:07:21 -0500

    feat(add-to-family-tree-view):squashed all commits into one commit
    removed backend

--- server/my-family-ws/src/main/java/com/bytepushers/family/GlobalErrorHandler/DatabaseOperationException.java
+++
@@ -1,7 +0,0 @@
-package com.bytepushers.family.GlobalErrorHandler;
-
-public class DatabaseOperationException extends RuntimeException {
-    public DatabaseOperationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
