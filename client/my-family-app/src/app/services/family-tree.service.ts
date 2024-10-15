36c690e3ef24448d0252dd829249cb4f93ea5dbf
Tonté Pouncil <tonte.pouncil@bytepushers.software>
2024-10-15 00:33:41 -0500

    feat(dockerized-project): dockerized project

--- client/my-family-app/src/app/services/family-tree.service.ts
+++ client/my-family-app/src/app/services/family-tree.service.ts
@@ -8,16 +8,30 @@ import { FamilyTreeRequestPayload } from '../models/family-tree-request.payload'
   providedIn: 'root'
 })
 export class FamilyTreeService {
-  private apiUrl = 'http://localhost:8100/api/v1/family-tree'; // Replace with actual API URL (ask whose working on it)
+  private apiUrl = 'http://localhost:8100/api/family-tree'; // Replace with actual API URL (ask whose working on it)
 
   constructor(private http: HttpClient) {}
 
   // Method to submit the family tree data
   public submitFamilyTree(payload: FamilyTreeRequestPayload): Observable<any> {
+    payload = {
+      parentName: "John you",
+      parentType: "no Father",
+      grandParentName: "Robert Doe",
+      grandParentType: "Grandfather",
+      siblingName: "Jane Doe",
+      siblingType: "Sister",
+      spouseName: "Mary Doe",
+      spouseType: "Wife",
+      childrenName: "Jack Doe",
+      childrenType: "Son"
+    } as FamilyTreeRequestPayload;
+
     const headers: HttpHeaders = new HttpHeaders({
       'Content-Type': 'application/json',
       'Accept': 'application/json',
-      'Accept-Version': '0.0.0.1' // Add more version numbers as needed
+      'Accept-Version': '0.0.0.1',
+      'Authorization': 'Basic ' + btoa('username:password')// Add more version numbers as needed
     });
 
     console.log(`payload: ${JSON.stringify(payload)}`, payload);
