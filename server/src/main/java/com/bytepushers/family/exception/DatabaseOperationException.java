abf897180b5cdae5b1e697e8b84595ebfd45005d
Tonté Pouncil <tonte.pouncil@bytepushers.software>
2024-10-17 23:55:31 -0500

    feat(create-account-api): refactored and fix after issue with last merge and PR#34

--- server/src/main/java/com/bytepushers/family/exception/DatabaseOperationException.java
+++
@@ -1,7 +0,0 @@
-package com.bytepushers.family.exception;
-
-public class DatabaseOperationException extends RuntimeException {
-    public DatabaseOperationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
