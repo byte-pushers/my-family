72f21ec881df81d8eca4d4743d3f232e836ed2b3
Tonté Pouncil <tonte.pouncil@bytepushers.software>
2025-01-15 16:01:10 -0600

    feat(re-factor-ui-domain-models): wip - started refactoring toward the final model structure

--- client/my-family-app/src/app/models/family-tree/family-tree.ts
+++ client/my-family-app/src/app/models/family-tree/family-tree.ts
@@ -1,6 +1,8 @@
 import { BaseDomain } from '../base-domain';
 import { FamilyMember } from './family-member';
+import { Person } from './person';
 
 export interface FamilyTree extends BaseDomain {
   familyMembers: FamilyMember[];
+  // people: Person[];
 }
