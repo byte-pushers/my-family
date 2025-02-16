88c72fe213b237175734e0b8d67c0a0616a7230d
Stella Choi <stellachoi120701@gmail.com>
2025-02-15 18:04:33 -0600

    feat(re-factor-app): wip test for adding spouse

--- client/my-family-app/src/app/models/family-tree/person.model.spec.ts
+++ client/my-family-app/src/app/models/family-tree/person.model.spec.ts
@@ -1,5 +1,6 @@
 import { Person } from './person';
 import { PersonModel } from './person.model';
+import { UnionModel } from "./union.model";
 
 describe('Person Domain Model', () => {
   const createdDate = '2024-10-16T10:00:00.000Z';
@@ -10,6 +11,16 @@ describe('Person Domain Model', () => {
   function generatePersonWithOneSibling(): Person {
     const siblings = [new PersonModel(undefined, 'James', 'Davis', undefined, 'Male', true)]
     const somePerson = new PersonModel(1, 'John', 'Davis', new Date('1970-01-01'), 'Male', false, siblings, null, null, createdBy, new Date(createdDate), updatedBy, new Date(updatedDate));
+
+    return somePerson;
+  }
+
+  function generatePersonWithSpouse(): Person {
+    // todo: create test case for spouse
+    const union = new UnionModel(1, true, new PersonModel(1, 'Tammy', 'Davis', new Date('1970-01-01'), 'Female', false, null, null, null, createdBy, new Date(createdDate), updatedBy, new Date(updatedDate)), null, createdBy, new Date(createdDate));
+    const somePerson = new PersonModel(1, 'John', 'Davis', new Date('1970-01-01'), 'Male', false, null, null, union, createdBy, new Date(createdDate), updatedBy, new Date(updatedDate));
+
+    console.log(`some person: ${somePerson}`);
 
     return somePerson;
   }
@@ -27,6 +38,19 @@ describe('Person Domain Model', () => {
     expect(actualPerson.siblings && actualPerson.siblings[0].firstName).toBe(expectedPerson.siblings[0].firstName);
     expect(actualPerson.siblings && actualPerson.siblings[0].deceased).toBe(expectedPerson.siblings[0].deceased);
   });
+
+  it('should be able to create person with a spouse', async () => {
+    const expectedPerson = JSON.parse(JSON.stringify(personWithASpouse)).default;
+    const expectFlattenPerson = JSON.stringify(expectedPerson).replace(/(\r\n|\r|\n|\t| )/gm, "");
+    const actualPerson = generatePersonWithSpouse();
+    const actualFlattenPerson = actualPerson.toString().replace(/(\r\n|\r|\n|\t| )/gm, "");
+
+    console.log(`expectFlattenPayload: "${expectFlattenPerson}"`);
+    console.log(`actualFlattenPayload: "${actualFlattenPerson}"`);
+
+    expect(actualFlattenPerson).toBe(expectFlattenPerson);
+    expect(actualPerson.unions).toBe(expectedPerson.unions);
+  })
 
   it('union array should be undefined if not specified.', async() => {
     const expectedPerson = JSON.parse(JSON.stringify(personWithNoUnion)).default;
@@ -63,4 +87,5 @@ describe('Person Domain Model', () => {
 
 import * as personWithASibling from '../../../../test/data/person.with.a.sibling.json';
 import * as personWithNoUnion from '../../../../test/data/person.with.no.union.json';
-import * as personWithEmptyUnion from '../../../../test/data/person.with.empty.union.json'
+import * as personWithEmptyUnion from '../../../../test/data/person.with.empty.union.json';
+import * as personWithASpouse from '../../../../test/data/person.with.a.spouse.json';
